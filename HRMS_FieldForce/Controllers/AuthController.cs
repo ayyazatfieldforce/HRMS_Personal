807cc44806b59f598f75d663f85bf289a219b4d2
M. Khushnood Saeed <105512217+Khushnood-Saeed@users.noreply.github.com>
2024-05-22 15:54:58 +0500

    JWT Implimented

--- HRMS_FieldForce/Controllers/AuthController.cs
+++ HRMS_FieldForce/Controllers/AuthController.cs
@@ -1,14 +1,27 @@
 ﻿using HRMS_FieldForce.Models;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace HRMS_FieldForce.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
+
+    
     public class AuthController : ControllerBase
     {
         public static User user = new User();
+
+        private readonly IConfiguration _configuration;
+
+        public AuthController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
 
         [HttpPost("register")]
 
@@ -34,11 +47,39 @@ namespace HRMS_FieldForce.Controllers
                 return BadRequest("User Name or Password is incorrect");
             }
 
-            return Ok(user);
+            var token = CreateToken(user);
+
+
+            return Ok(token);
 
 
 
         }
+
+        private string CreateToken(User user)
+        {
+            List<Claim> claims = new List<Claim> {
+
+            new Claim(ClaimTypes.Name,user.UserName)
+
+            };
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!));
+
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+
+            var token = new JwtSecurityToken(
+                claims: claims,
+                expires: DateTime.Now.AddDays(1),
+                signingCredentials:creds
+                );  
+
+            var jwt=new JwtSecurityTokenHandler().WriteToken(token);
+
+            return jwt;
+            
+        }
+
 
 
     }
