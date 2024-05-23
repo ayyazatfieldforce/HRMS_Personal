f25ac4f11adf14d3058327644588262945fb3af6
M. Khushnood Saeed <105512217+Khushnood-Saeed@users.noreply.github.com>
2024-05-23 16:03:43 +0500

    Partial Attendence Module  with User Details

---
+++ HRMS_FieldForce/Models/Attendence.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+using System.ComponentModel.DataAnnotations;
+
+namespace HRMS_FieldForce.Models
+{
+    public class Attendence
+    {
+        public class Attendance
+        {
+            //User and Date will make up Composite Primary Key
+            [Required]
+            [ForeignKey("User")]
+            public required string UserId { get; set; }
+
+            [Required]
+            public required DateTime Date { get; set; }
+
+            public DateTime? CheckInTime { get; set; }
+
+            public DateTime? CheckOutTime { get; set; }
+
+            [StringLength(10)]
+            public string? WorkFrom { get; set; }
+
+         
+            public User? User { get; set; }
+        }
+    }
+}
