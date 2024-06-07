e06784794f6f7bdb20269c8fcd3b0732a60f1a1c
ayyazatfieldforce <ayyaz.sohail@getfieldforce.com>
2024-06-07 13:11:06 +0500

    good ha

--- HRMS_FieldForce/Models/LeaveCategory.cs
+++ HRMS_FieldForce/Models/LeaveCategory.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HRMS_FieldForce.Models
 {
     public class LeaveCategory
     {
         [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
--- HRMS_FieldForce/Models/LeaveType.cs
+++ HRMS_FieldForce/Models/LeaveType.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace HRMS_FieldForce.Models
 {
     public class LeaveType
     {
         [Required]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
 
         [Required]
