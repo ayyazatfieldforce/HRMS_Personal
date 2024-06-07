7c7585e3ca605b38eb066e41fdc71ce8948ae9ba
M. Khushnood Saeed <105512217+Khushnood-Saeed@users.noreply.github.com>
2024-06-07 18:21:56 +0500

    RoleBasedAccessMW

---
+++ HRMS_FieldForce/Middlewares/RoleBasedAccessMiddleware.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.IO;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+namespace HRMS_FieldForce.Middlewares
+{
+    public class RoleBasedAccessMiddleware
+    {
+        private readonly RequestDelegate _next;
+        private readonly IConfiguration _configuration;
+
+        public RoleBasedAccessMiddleware(RequestDelegate next, IConfiguration configuration)
+        {
+            _next = next;
+            _configuration = configuration;
+        }
+
+        public async Task Invoke(HttpContext context)
+        {
+            var path = context.Request.Path.Value;
+            if (path.StartsWith("/swagger") || path.StartsWith("/api-docs")|| path.StartsWith("/api/Auth/login"))
+            {
+                await _next(context);
+                return;
+            }
+            var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+            if (token != null)
+            {
+                try
+                {
+                    var key = Encoding.UTF8.GetBytes(_configuration.GetSection("AppSettings:Token").Value!);
+                    var tokenHandler = new JwtSecurityTokenHandler();
+                    var claims = tokenHandler.ValidateToken(token, new TokenValidationParameters
+                    {
+                        ValidateIssuerSigningKey = true,
+                        IssuerSigningKey = new SymmetricSecurityKey(key),
+                        ValidateIssuer = false,
+                        ValidateAudience = false,
+                        ClockSkew = TimeSpan.Zero
+                    }, out SecurityToken validatedToken);
+
+                    var jwtToken = (JwtSecurityToken)validatedToken;
+                    var role = jwtToken.Claims.First(x => x.Type == ClaimTypes.Role).Value;
+                    var userId = jwtToken.Claims.First(x => x.Type == ClaimTypes.NameIdentifier).Value;
+
+                    context.Items["Role"] = role;
+                    context.Items["UserId"] = userId;
+                }
+                catch
+                {
+                    context.Response.StatusCode = 401;
+                    await context.Response.WriteAsync("Invalid Token");
+                    return;
+                }
+            }
+            else
+            {
+                context.Response.StatusCode = 401;
+                await context.Response.WriteAsync("Token is missing");
+                return;
+            }
+
+            await _next(context);
+        }
+    }
+}
--- HRMS_FieldForce/Program.cs
+++ HRMS_FieldForce/Program.cs
@@ -1,5 +1,7 @@
 using HRMS_FieldForce.Data;
+using HRMS_FieldForce.Middlewares;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,7 +10,40 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "My API", Version = "v1" });
+
+    // Add JWT Authentication
+    c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+    {
+        Description = @"JWT Authorization header using the Bearer scheme. \r\n\r\n 
+                      Enter 'Bearer' [space] and then your token in the text input below.
+                      \r\n\r\nExample: 'Bearer 12345abcdef'",
+        Name = "Authorization",
+        In = ParameterLocation.Header,
+        Type = SecuritySchemeType.ApiKey,
+        Scheme = "Bearer"
+    });
+
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement()
+    {
+        {
+            new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
+                {
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                },
+                Scheme = "oauth2",
+                Name = "Bearer",
+                In = ParameterLocation.Header,
+            },
+            new List<string>()
+        }
+    });
+});
 
 
 var ConnectionString = builder.Configuration.GetConnectionString("MySQLConnection");
@@ -19,6 +54,7 @@ builder.Services.AddDbContext<DataContext>(options =>
 });
 
 var app = builder.Build();
+app.UseMiddleware<RoleBasedAccessMiddleware>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
